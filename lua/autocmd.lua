02054ba14fc39a7847c6ac73f54d4ca0029d64d9
Thorsten Rhau <thorsten@rhau.se>
2023-03-02 19:11:25 +0100

    Now closing 'checkhealth' with q

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -50,6 +50,7 @@ vim.api.nvim_create_autocmd("FileType", {
     "tsplayground",
     "PlenaryTestPopup",
     "Git",
+    "checkhealth",
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
