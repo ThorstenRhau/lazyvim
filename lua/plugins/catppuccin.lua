8eed76e949ed2bdb27562a3b9ce171ec7cced626
Thorsten Rhau <thorsten@rhau.se>
2023-03-02 20:47:24 +0100

    Added 'catppuccin' themne

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,73 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = true,
+    run = "compile",
+    opts = {
+      compile_path = vim.fn.stdpath "cache" .. "/catppuccin",
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+      term_colors = false,
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        telescope = true,
+        notify = false,
+        mini = false,
+        fidget= true,
+        lsp_saga = true,
+        mason = true,
+        neotree = true,
+        treesitter_context = true,
+        treesitter = true,
+        lsp_trouble = true,
+        which_key = true,
+        native_lsp = {
+          enabled = true,
+          virtual_text = {
+            errors = { "italic" },
+            hints = { "italic" },
+            warnings = { "italic" },
+            information = { "italic" },
+          },
+          underlines = {
+            errors = { "underline" },
+            hints = { "underline" },
+            warnings = { "underline" },
+            information = { "underline" },
+          },
+        },
+      },
+    },
+  },
+}
