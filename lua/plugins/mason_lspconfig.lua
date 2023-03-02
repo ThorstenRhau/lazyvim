f21d972b7404e19c642f7b506e66155eb64567ea
Thorsten Rhau <thorsten@rhau.se>
2023-03-02 20:07:53 +0100

    Streamlined configuration for mason-lspconfig

--- lua/plugins/mason_lspconfig.lua
+++ lua/plugins/mason_lspconfig.lua
@@ -31,53 +31,9 @@ return {
         automatic_installation = true,
       })
 
-      local status_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-      if not status_ok then
-        vim.notify("Problem with mason-lspconfig")
-        return
-      end
-
-      ---@diagnostic disable-next-line: unused-local
-      local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
-      if not status_ok then
-        vim.notify("Problems with lspconfig")
-        return
-      end
-
-      ---@diagnostic disable-next-line: unused-local
-      local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        -- local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-        -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-        -- vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-        -- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-        -- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
-        -- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-        -- vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-        -- vim.keymap.set("n", "<space>wl", function()
-        --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        -- end, bufopts)
-        -- vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-        -- vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-        -- vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-        -- vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-        -- vim.keymap.set("n", "<space>F", vim.lsp.buf.formatting, bufopts)
-      end
-
-      mason_lspconfig.setup_handlers({
-
-        -- This is a default handler that will be called for each installed server
-        -- (also for new servers that are installed during a session)
-        function(server_name)
-          lspconfig[server_name].setup({
-            on_attach = on_attach,
-            flags = lsp_flags,
-          })
+      require("mason-lspconfig").setup_handlers({
+        function(server_name) -- default handler (optional)
+          require("lspconfig")[server_name].setup({})
         end,
       })
     end,
