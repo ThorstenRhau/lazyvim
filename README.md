8d126504c9ebe9dec00bab47c98ac6fcf4c9a08a
Thorsten Rhau <thorsten@rhau.se>
2023-03-03 19:18:54 +0100

    Rewrote some of the null-ls config, updated README

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Thorsten's lazyvim config
-Shortly after I finished my Packer based configuration of neovim I realised  that I wanted to switch to the lazy.nvim packet manager. So here is another repo with very similar configuration.
 
 This repo is intended for my personal use, it is public for easier cloning.
 
--- lua/plugins/nullls.lua
+++ lua/plugins/nullls.lua
@@ -6,16 +6,45 @@ return {
       "jay-babu/mason-null-ls.nvim",
     },
     opts = function()
-      local null_ls = require("null-ls")
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/blob/main/doc/BUILTINS.md
+      local present, null_ls = pcall(require, "null-ls")
+
+      if not present then
+        return
+      end
+
+      local b = null_ls.builtins
+
+      local sources = {
+        -- format html and markdown
+        b.formatting.prettier.with({ filetypes = { "html", "yaml", "markdown" } }),
+        b.formatting.isort, -- Python
+        b.formatting.autopep8, -- Python
+        b.diagnostics.pylint, -- Python
+        b.formatting.shfmt, -- Shell script
+        b.formatting.stylua, -- Lua
+        b.formatting.prettier, -- HTML et.al.
+      }
+
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+      local on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+              --vim.lsp.buf.formatting_sync()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end
+
       null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.isort, -- Python
-          null_ls.builtins.formatting.autopep8, -- Python
-          null_ls.builtins.formatting.shfmt, -- Shell script
-          null_ls.builtins.formatting.stylua, -- Lua
-          null_ls.builtins.formatting.prettier, -- HTML et.al.
-        },
+        debug = true,
+        sources = sources,
+        on_attach = on_attach,
       })
     end,
   },
@@ -31,6 +60,7 @@ return {
           "shfmt",
           "stylua",
           "prettier",
+          "pylint",
         },
       })
     end,
